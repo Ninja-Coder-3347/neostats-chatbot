e3849cc9a5cab3493e53cc92d1f6b4dc58d8467f
Ninja-Coder-3347 <pallavisuryawanshi3347@gmail.com>
2026-03-13 23:59:30 +0530

    Implemented RAG document retrieval

---
+++ utils/rag_utils.py
@@ -0,0 +1,31 @@
+from langchain.document_loaders import PyPDFLoader
+from langchain.vectorstores import FAISS
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from models.embeddings import load_embedding_model
+
+
+def create_vector_store(file_path):
+
+    try:
+
+        loader = PyPDFLoader(file_path)
+        documents = loader.load()
+
+        splitter = RecursiveCharacterTextSplitter(
+            chunk_size=500,
+            chunk_overlap=50
+        )
+
+        chunks = splitter.split_documents(documents)
+
+        embedding_model = load_embedding_model()
+
+        vector_store = FAISS.from_documents(
+            chunks,
+            embedding_model
+        )
+
+        return vector_store
+
+    except Exception as e:
+        print(e)
