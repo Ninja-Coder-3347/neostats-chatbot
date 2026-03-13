4a6a0fdc389877111c6fb961cc7a2cf1dbf73fd4
Ninja-Coder-3347 <pallavisuryawanshi3347@gmail.com>
2026-03-13 23:50:03 +0530

    Implemented LLM model loading

---
+++ models/llm.py
@@ -0,0 +1,40 @@
+from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_groq import ChatGroq
+from config.config import OPENAI_API_KEY, GEMINI_API_KEY, GROQ_API_KEY
+
+
+def load_openai():
+
+    try:
+        return ChatOpenAI(
+            api_key=OPENAI_API_KEY,
+            model="gpt-4o-mini"
+        )
+
+    except Exception as e:
+        print(e)
+
+
+def load_gemini():
+
+    try:
+        return ChatGoogleGenerativeAI(
+            google_api_key=GEMINI_API_KEY,
+            model="gemini-pro"
+        )
+
+    except Exception as e:
+        print(e)
+
+
+def load_groq():
+
+    try:
+        return ChatGroq(
+            api_key=GROQ_API_KEY,
+            model="llama3-8b-8192"
+        )
+
+    except Exception as e:
+        print(e)
